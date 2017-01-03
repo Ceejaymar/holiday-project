d1e1e22059191e432af588d3212d8a0563d178c6
Carlos Martinez <martinezcj2@gmail.com>
2017-01-03 14:55:40 -0500

    styled CreateProject page

--- frontend/components/projects/CreateProject.jsx
+++ frontend/components/projects/CreateProject.jsx
@@ -27,15 +27,17 @@ const CreateProject = React.createClass({
   },
   render: function(){
     return (
-      <div>
-        This the create project page
-        <form onSubmit={this.submit}>
-          <input type="text" placeholder="title" value={this.state.title} onChange={this.handleChange.bind(this, "title")}></input>
-          <input type="text" placeholder="image" value={this.state.image} onChange={this.handleChange.bind(this, "image")}></input>
-          <input type="text" placeholder="github" value={this.state.github} onChange={this.handleChange.bind(this, "github")}></input>
-          <input type="text" placeholder="description" value={this.state.description} onChange={this.handleChange.bind(this, "description")}></input>
-          <input type="submit"></input>
-        </form>
+      <div className="createProjectCont">
+        <h1>Add a project</h1>
+        <div className="formCont">
+          <form onSubmit={this.submit}>
+            <input type="text" placeholder="title" value={this.state.title} onChange={this.handleChange.bind(this, "title")}></input>
+            <input type="text" placeholder="image" value={this.state.image} onChange={this.handleChange.bind(this, "image")}></input>
+            <input type="text" placeholder="github" value={this.state.github} onChange={this.handleChange.bind(this, "github")}></input>
+            <input type="text" placeholder="description" value={this.state.description} onChange={this.handleChange.bind(this, "description")}></input>
+            <input className="submitButton" type="submit"></input>
+          </form>
+        </div>
       </div>
     )
   }
